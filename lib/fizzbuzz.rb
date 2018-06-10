9e2eeaf93e4d0d9572ba7bfab40ebfe99c09d7b9
Benjamin <ben.smith.gordon@gmail.com>
2018-06-10 13:49:14 +0100

    Tested that it doesn't return fizz when number isn't 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(number)
-  "fizz"
-
+  if number % 3 == 0
+    "fizz"
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,12 @@
 require "fizzbuzz"
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it "doesn't return fizz if passed anything other than 3" do
+    expect(fizzbuzz(4)).to_not eq "fizz"
+  end
 end
+
