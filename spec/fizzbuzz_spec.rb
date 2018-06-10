1496471f9ccfddd058ace8419425b8f18467695e
Kate Beavis <katebeavis84@gmail.com>
2018-06-10 13:55:10 +0100

    Tests passing for buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,5 +8,13 @@ describe 'fizzbuzz' do
   it "doesn't return fizz if passed anything other than 3" do
     expect(fizzbuzz(4)).to_not eq "fizz"
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it "doesn't return 'buzz' if passed anything other than 5" do
+    expect(fizzbuzz(6)).to_not eq 'buzz'
+  end
 end
 
